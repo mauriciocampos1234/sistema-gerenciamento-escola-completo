222037052800d2039faec979bec06a7d900a4141
mauriciocampos1234 <mauriciocampospaypal@gmail.com>
2025-10-05 10:57:59 -0300

    Criando a tela de login

---
+++ src/SistemaEscolar.web/Controllers/UsuarioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaEscolar.web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
