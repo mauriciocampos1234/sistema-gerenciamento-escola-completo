9385413f4d7e844cbf3a1a836d74e9d332b4763d
mauriciocampos1234 <mauriciocampospaypal@gmail.com>
2025-10-07 12:55:33 -0300

    Correção do ultimo commit devido a uma atualização da maquina

---
+++ src/SistemaEscolar.Services/Models/Usuario/ValidarLoginResult.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaEscolar.Services.Models.Usuario
+{
+    public class ValidarLoginResult
+    {
+        public bool Sucesso { get; set; }
+
+        public string? MensagemErro { get; set; } // Pode ser nulo se Sucesso for true
+    }
+}
---
+++ src/SistemaEscolar.Services/UsuarioService.cs
@@ -0,0 +1,16 @@
+﻿using SistemaEscolar.Services.Models.Usuario;
+
+namespace SistemaEscolar.Services
+{
+    public class UsuarioService
+    {
+        public ValidarLoginResult ValidarLogin(string email, string senha)
+        {
+            var result = new ValidarLoginResult();
+
+            result.Sucesso = true;
+
+            return result;
+        }
+    }
+}
