a9909fe0d8940cb505e2a554009ecb10d3f7b09f
mauriciocampos1234 <mauriciocampospaypal@gmail.com>
2025-10-25 17:24:53 -0300

    Feito Menu Funcional

---
+++ src/SistemaEscolar.web/Components/MenuViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaEscolar.web.Models.Menu;
+
+namespace SistemaEscolar.web.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var menu = new MenuViewModel
+            {
+                Ativo = ViewData["Menu"] as Menu? ?? Menu.Home
+            };
+
+            return View(menu);
+        }
+    }
+}
---
+++ src/SistemaEscolar.web/Models/Menu/MenuViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SistemaEscolar.web.Models.Menu
+{
+    public class MenuViewModel
+    {
+        public Menu Ativo { get; set; }
+    }
+
+    //Enum para o menu
+    public enum Menu
+    { 
+        Home,
+        Professor,
+        Aluno,
+        Turma
+    }
+}
