591c3d9b833a0159cd1e821798f9a12e3ce1a1a6
mauriciocampos1234 <mauriciocampospaypal@gmail.com>
2025-11-13 16:54:29 -0300

    1 passo

---
+++ src/SistemaEscolar.Repositories/SqlServerRepositoryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+
+
+namespace SistemaEscolar.Repositories
+{
+    public static class SqlServerRepositoryExtensions
+    {
+        public static decimal? GetDecimalOrNull(this SqlDataReader reader, string name)
+        {
+            return reader[name] == DBNull.Value ? null : (decimal)reader[name];
+        }
+
+        public static int? GetInt32OrNull(this SqlDataReader reader, string name)
+        {
+            return reader[name] == DBNull.Value ? null : (int)reader[name];
+        }
+    }
+}
